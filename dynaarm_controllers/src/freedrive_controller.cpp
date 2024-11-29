f3deb69388b46d904503c61b4bc967d109b07cf0
Lennart Nachtigall <mail@firesurfer.de>
2024-11-29 15:50:56 +0100

    added very simple freedrive controller

---
+++ dynaarm_controllers/src/freedrive_controller.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2024 Duatic AG
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dynaarm_controllers/freedrive_controller.hpp"
+
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
+#include <controller_interface/helpers.hpp>
+#include <lifecycle_msgs/msg/state.hpp>
+
+namespace dynaarm_controllers
+{
+FreeDriveController::FreeDriveController()
+{
+}
+controller_interface::InterfaceConfiguration FreeDriveController::command_interface_configuration() const
+{
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+   const auto joints = params_.joints;
+  for (auto& joint : joints) {
+    config.names.emplace_back(joint + "/" + hardware_interface::HW_IF_POSITION);
+  }
+  return config;
+};
+
+controller_interface::InterfaceConfiguration FreeDriveController::state_interface_configuration() const {
+      // Claim the necessary state interfaces
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+  const auto joints = params_.joints;
+  for (auto& joint : joints) {
+    config.names.emplace_back(joint + "/" + hardware_interface::HW_IF_POSITION);
+  }
+  return config;
+};
+
+controller_interface::CallbackReturn FreeDriveController::on_init()
+{
+  try {
+    // Obtains necessary parameters
+    param_listener_ = std::make_unique<freedrive_controller::ParamListener>(get_node());
+    param_listener_->refresh_dynamic_parameters();
+    params_ = param_listener_->get_params();
+  } catch (const std::exception& e) {
+    RCLCPP_ERROR_STREAM(get_node()->get_logger(), "Exception during controller init: " << e.what());
+    return CallbackReturn::ERROR;
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
+};
+
+controller_interface::CallbackReturn FreeDriveController::on_configure([[maybe_unused]] const rclcpp_lifecycle::State& previous_state)
+{
+  // update the dynamic map parameters
+  param_listener_->refresh_dynamic_parameters();
+
+  // get parameters from the listener in case they were updated
+  params_ = param_listener_->get_params();
+
+  // check if joints are empty
+  if (params_.joints.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'joints' parameter is empty.");
+    return controller_interface::CallbackReturn::FAILURE;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+};
+
+controller_interface::CallbackReturn FreeDriveController::on_activate([[maybe_unused]] const rclcpp_lifecycle::State& previous_state)
+{
+    active_ = true;
+  return controller_interface::CallbackReturn::SUCCESS;
+};
+
+controller_interface::CallbackReturn FreeDriveController::on_deactivate([[maybe_unused]] const rclcpp_lifecycle::State& previous_state)
+{
+    active_ = false;
+  return controller_interface::CallbackReturn::SUCCESS;
+};
+
+controller_interface::return_type FreeDriveController::update([[maybe_unused]] const rclcpp::Time& time,[[maybe_unused]] const rclcpp::Duration& period)
+{   
+    // This only works as long as we are only claming joint interfaces
+    for(std::size_t i = 0; i< state_interfaces_.size();i++){
+        if(!command_interfaces_.at(i).set_value(state_interfaces_.at(i).get_value())){
+            RCLCPP_ERROR_STREAM(get_node()->get_logger(), "Error wring value to command interface: " << command_interfaces_.at(i).get_name());
+                 return controller_interface::return_type::ERROR;
+        }
+    }
+  return controller_interface::return_type::OK;
+};
+
+}  // namespace dynaarm_controllers
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(dynaarm_controllers::FreeDriveController, controller_interface::ControllerInterface)
