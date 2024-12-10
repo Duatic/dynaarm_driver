1cb3f4ef2db4f2cd074115c876b5896acc88e912
Lennart Nachtigall <mail@firesurfer.de>
2024-12-10 08:21:59 +0100

    added base stub for controller

---
+++ dynaarm_controllers/src/dynaarm_pid_controller.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2024 Duatic AG
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dynaarm_controllers/dynaarm_pid_controller.hpp"
+
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
+#include <controller_interface/helpers.hpp>
+#include <lifecycle_msgs/msg/state.hpp>
+
+namespace dynaarm_controllers
+{
+PIDController::PIDController()
+{
+}
+controller_interface::InterfaceConfiguration PIDController::command_interface_configuration() const
+{
+  // Claim the necessary command interfaces
+
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  return config;
+}
+
+controller_interface::InterfaceConfiguration PIDController::state_interface_configuration() const
+{
+  // Claim the necessary state interfaces
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  return config;
+}
+
+controller_interface::CallbackReturn PIDController::on_init()
+{
+  try {
+    // Obtains necessary parameters
+    param_listener_ = std::make_unique<dynaarm_pid_controller::ParamListener>(get_node());
+    param_listener_->refresh_dynamic_parameters();
+    params_ = param_listener_->get_params();
+  } catch (const std::exception& e) {
+    RCLCPP_ERROR_STREAM(get_node()->get_logger(), "Exception during controller init: " << e.what());
+    return CallbackReturn::ERROR;
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn PIDController::on_configure(const rclcpp_lifecycle::State& previous_state)
+{
+  // update the dynamic map parameters
+  param_listener_->refresh_dynamic_parameters();
+
+  // get parameters from the listener in case they were updated
+  params_ = param_listener_->get_params();
+
+  // check if joints are empty
+  if (params_.joints.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'joints' parameter is empty.");
+    return controller_interface::CallbackReturn::FAILURE;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn PIDController::on_activate(const rclcpp_lifecycle::State& previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn PIDController::on_deactivate(const rclcpp_lifecycle::State& previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type PIDController::update(const rclcpp::Time& time, const rclcpp::Duration& period)
+{
+  if (get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE) {
+    return controller_interface::return_type::OK;
+  }
+  return controller_interface::return_type::OK;
+}
+}  // namespace dynaarm_controllers
