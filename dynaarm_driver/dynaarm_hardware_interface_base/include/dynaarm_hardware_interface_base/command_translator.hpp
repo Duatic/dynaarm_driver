b9083d11124af6e985c725d8d066b49c5aaf2cb3
Schwimo <tschwarzer@software-byts.de>
2024-12-10 17:52:32 +0000

    please pre-commit

--- dynaarm_driver/dynaarm_hardware_interface_base/include/dynaarm_hardware_interface_base/command_translator.hpp
+++ dynaarm_driver/dynaarm_hardware_interface_base/include/dynaarm_hardware_interface_base/command_translator.hpp
@@ -30,108 +30,112 @@
 namespace dynaarm_hardware_interface_common
 {
 
-    class CommandTranslator
-    {
-    public:
-        CommandTranslator() = default;
+class CommandTranslator
+{
+public:
+  CommandTranslator() = default;
 
-        virtual ~CommandTranslator() = default;
+  virtual ~CommandTranslator() = default;
 
-        /*!
-         * Compute the mapping from the absolute angles of the dynaarm to the relative angles used in the Ocs2 convention.
-         * theta = relative angles
-         * q = absolute angles
-         * theta = mappingFromAbsoluteToRelativeAngles * q
-         */
-        static Eigen::VectorXd mapFromDynaarmToSerialCoordinates(const Eigen::VectorXd &input)
-        {
-            int size = input.size();
-            if (size < 4)
-            {
-                throw std::invalid_argument("Input vector must have at least 4 elements");
-            }
+  /*!
+   * Compute the mapping from the absolute angles of the dynaarm to the relative angles used in the Ocs2 convention.
+   * theta = relative angles
+   * q = absolute angles
+   * theta = mappingFromAbsoluteToRelativeAngles * q
+   */
+  static Eigen::VectorXd mapFromDynaarmToSerialCoordinates(const Eigen::VectorXd& input)
+  {
+    int size = input.size();
+    if (size < 4) {
+      throw std::invalid_argument("Input vector must have at least 4 elements");
+    }
 
-            // Create a dynamic matrix with size 'size'
-            Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
+    // Create a dynamic matrix with size 'size'
+    Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
 
+    // clang-format off
             // Modify the first 4x4 submatrix
             mapping.block<4, 4>(0, 0) << 1.0, 0.0, 0.0, 0.0,
                                         0.0, 1.0, 0.0, 0.0,
                                         0.0, -1.0, 1.0, 0.0,
                                         0.0, 0.0, 0.0, 1.0;
+    // clang-format on
 
-            // Return the transformed vector
-            return mapping * input;
-        }
+    // Return the transformed vector
+    return mapping * input;
+  }
 
-        static Eigen::VectorXd mapFromDynaarmToSerialTorques(const Eigen::VectorXd &input)
-        {
-            int size = input.size();
-            if (size < 4)
-            {
-                throw std::invalid_argument("Input vector must have at least 4 elements");
-            }
+  static Eigen::VectorXd mapFromDynaarmToSerialTorques(const Eigen::VectorXd& input)
+  {
+    int size = input.size();
+    if (size < 4) {
+      throw std::invalid_argument("Input vector must have at least 4 elements");
+    }
 
-            // Create a dynamic matrix with size 'size'
-            Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
+    // Create a dynamic matrix with size 'size'
+    Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
 
+    // clang-format off
             // Modify the first 4x4 submatrix
             mapping.block<4, 4>(0, 0) << 1.0, 0.0, 0.0, 0.0,
                 0.0, 1.0, 1.0, 0.0,
                 0.0, 0.0, 1.0, 0.0,
                 0.0, 0.0, 0.0, 1.0;
+    // clang-format on
 
-            // Return the transformed vector
-            return mapping * input;
-        }
+    // Return the transformed vector
+    return mapping * input;
+  }
 
-        /*!
-         * Compute the mapping from the relative angles of the ocs2_convention to the absolute angles used for the dynaarm
-         * theta = relative angles
-         * q = absolute angles
-         * q = mappingFromRelativeToAbsoluteAngles * theta
-         */
-        static Eigen::VectorXd mapFromSerialToDynaarmCoordinates(const Eigen::VectorXd &input)
-        {
-            int size = input.size();
-            if (size < 4)
-            {
-                throw std::invalid_argument("Input vector must have at least 4 elements");
-            }
+  /*!
+   * Compute the mapping from the relative angles of the ocs2_convention to the absolute angles used for the dynaarm
+   * theta = relative angles
+   * q = absolute angles
+   * q = mappingFromRelativeToAbsoluteAngles * theta
+   */
+  static Eigen::VectorXd mapFromSerialToDynaarmCoordinates(const Eigen::VectorXd& input)
+  {
+    int size = input.size();
+    if (size < 4) {
+      throw std::invalid_argument("Input vector must have at least 4 elements");
+    }
 
-            // Create a dynamic matrix with size 'size'
-            Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
+    // Create a dynamic matrix with size 'size'
+    Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
 
+    // clang-format off
             // Modify the first 4x4 submatrix
             mapping.block<4, 4>(0, 0) << 1.0, 0.0, 0.0, 0.0,
                 0.0, 1.0, 0.0, 0.0,
                 0.0, 1.0, 1.0, 0.0,
                 0.0, 0.0, 0.0, 1.0;
+    // clang-format on
 
-            // Return the transformed vector
-            return mapping * input;
-        }
+    // Return the transformed vector
+    return mapping * input;
+  }
 
-        static Eigen::VectorXd mapFromSerialToDynaarmTorques(const Eigen::VectorXd &input)
-        {
-            int size = input.size();
-            if (size < 4)
-            {
-                throw std::invalid_argument("Input vector must have at least 4 elements");
-            }
+  static Eigen::VectorXd mapFromSerialToDynaarmTorques(const Eigen::VectorXd& input)
+  {
+    int size = input.size();
+    if (size < 4) {
+      throw std::invalid_argument("Input vector must have at least 4 elements");
+    }
 
-            // Create a dynamic matrix with size 'size'
-            Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
+    // Create a dynamic matrix with size 'size'
+    Eigen::MatrixXd mapping = Eigen::MatrixXd::Identity(size, size);
 
+    // clang-format off
             // Modify the first 4x4 submatrix
             mapping.block<4, 4>(0, 0) << 1.0, 0.0, 0.0, 0.0,
                 0.0, 1.0, -1.0, 0.0,
                 0.0, 0.0, 1.0, 0.0,
                 0.0, 0.0, 0.0, 1.0;
+    // clang-format on
 
-            // Return the transformed vector
-            return mapping * input;
-        }
-    };
+    // Return the transformed vector
+    return mapping * input;
+  }
+};
 
-} // namespace
+}  // namespace dynaarm_hardware_interface_common
