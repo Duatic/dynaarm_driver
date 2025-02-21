816d80e018c23e65ef7228103f501abbe97b76ee
Schwimo <timo.schwarzer@gmail.com>
2025-02-21 13:41:37 +0100

    Pleased pre-commit

--- dynaarm_extensions/setup.py
+++ dynaarm_extensions/setup.py
@@ -1,25 +1,24 @@
 from setuptools import find_packages, setup
 
-package_name = 'dynaarm_extensions'
+package_name = "dynaarm_extensions"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='timo.schwarzer@gmail.com',
-    description='DynaArm Extensions - E-Stop and other utilities',
-    license='TODO: License declaration',    
+    maintainer="root",
+    maintainer_email="timo.schwarzer@gmail.com",
+    description="DynaArm Extensions - E-Stop and other utilities",
+    license="TODO: License declaration",
     entry_points={
-        'console_scripts': [
-            'e_stop_node = dynaarm_extensions.e_stop.e_stop_node:main',
+        "console_scripts": [
+            "e_stop_node = dynaarm_extensions.e_stop.e_stop_node:main",
         ],
     },
 )
